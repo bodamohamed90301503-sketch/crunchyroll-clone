c4008a010f14f69cedbc42c737546ed9267ef31d
ABDO <bodamohamed90301503@gmail.com>
2026-05-25 21:43:00 +0300

    fix all anime imports to relative paths

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { getAnimeById } from "@/api/anime";
+import { getAnimeById } from "../../../api/anime";
 import CommentsClient from "./CommentsClient";
 
 type Props = {
@@ -18,7 +18,10 @@ export default async function Page({ params }: Props) {
         <div className="text-white p-6">
             <h1>{anime.title.english || anime.title.romaji}</h1>
 
-            <img src={anime.coverImage.large} alt={anime.title.romaji} />
+            <img
+                src={anime.coverImage.large}
+                alt={anime.title.romaji}
+            />
 
             {anime.description && (
                 <p dangerouslySetInnerHTML={{ __html: anime.description }} />
--- src/components/AnimeCarousel.tsx
+++ src/components/AnimeCarousel.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import { getAnime, type Anime } from "@/api/anime";
+import { getAnime, type Anime } from "../api/anime";
 
 export default function AnimeCarousel() {
     const [anime, setAnime] = useState<Anime[]>([]);
